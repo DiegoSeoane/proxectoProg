45be593fb986fbad8b76143d0887f6c412956bf3
ilizor <diegoseoges@gmail.com>
2023-05-17 21:44:49 +0200

    prueba de añadido de imagen

--- src/main/java/com/a22diegosg/proxectoprog/BoxeadorView.java
+++ src/main/java/com/a22diegosg/proxectoprog/BoxeadorView.java
@@ -5,9 +5,11 @@
 package com.a22diegosg.proxectoprog;
 
 import java.awt.*;
+import java.io.*;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
@@ -17,11 +19,12 @@ import javax.swing.*;
 public class BoxeadorView extends JFrame {
 
     private static final String DRIVER = "org.h2.Driver";
-    private static final String URL = "jdbc:h2:\\bd\\boxeador";
+    private static final String URL = "jdbc:h2:D:\\_Clase\\A22_Programacion\\proxectoProg\\src\\main\\resources\\bd\\boxeador";
 
     private ResultSet rs;
+    private JPanel panelFoto = new JPanel();
 
-    JPanel panelcentral = new JPanel(new GridLayout(5, 2, 10, 5));
+    JPanel panelDatos = new JPanel(new GridLayout(1, 3, 10, 5));
 
     public BoxeadorView(String title) throws HeadlessException {
         super(title);
@@ -37,38 +40,117 @@ public class BoxeadorView extends JFrame {
         add(new JPanel(), BorderLayout.PAGE_START);
         add(new JPanel(), BorderLayout.PAGE_END);
         setValores();
-        add(new JPanel(), BorderLayout.EAST);
-        add(new JPanel(), BorderLayout.WEST);
     }
 
-    public void setValores() {
+//    public void setValores() {
 //        try {
-            add(panelcentral, BorderLayout.CENTER);
-            panelcentral.add(new JLabel(new ImageIcon(
-                    BoxeadorView.class.getResource("/images/nombre.png"))));
-            panelcentral.add(new JLabel("nombre"));
-            //panelcentral.add(new JLabel(rs.getString("nombre")));
-            panelcentral.add(new JLabel(new ImageIcon(
-                    BoxeadorView.class.getResource("/images/mundo.png"))));
-            panelcentral.add(new JLabel("nacionalidad"));
-            //panelcentral.add(new JLabel(rs.getString("nacionalidad")));
-            panelcentral.add(new JLabel(new ImageIcon(
-                    BoxeadorView.class.getResource("/images/peleas.png"))));
-            panelcentral.add(new JLabel("peleas"));
-            //panelcentral.add(new JLabel(rs.getString("peleas")));
-            panelcentral.add(new JLabel(new ImageIcon(
-                    BoxeadorView.class.getResource("/images/win.png"))));
-            panelcentral.add(new JLabel("ganadas"));
-            //panelcentral.add(new JLabel(rs.getString("ganadas")));
-            panelcentral.add(new JLabel(new ImageIcon(
-                    BoxeadorView.class.getResource("/images/lose.png"))));
-            panelcentral.add(new JLabel("perdidas"));
-            //panelcentral.add(new JLabel(rs.getString("perdidas")));
+//            if (rs != null && !rs.isClosed()
+//                    && !rs.isBeforeFirst() && !rs.isAfterLast()) {
+//                add(panelDatos, BorderLayout.EAST);
+//                panelDatos.add(new JLabel(new ImageIcon(
+//                        BoxeadorView.class.getResource("/images/nombre.png"))));
+////            panelcentral.add(new JLabel("nombre"));
+//                panelDatos.add(new JLabel(rs.getString("nombre")));
+//                panelDatos.add(new JLabel(new ImageIcon(
+//                        BoxeadorView.class.getResource("/images/mundo.png"))));
+////            panelcentral.add(new JLabel("nacionalidad"));
+//                panelDatos.add(new JLabel(rs.getString("nacionalidad")));
+//                panelDatos.add(new JLabel(new ImageIcon(
+//                        BoxeadorView.class.getResource("/images/peleas.png"))));
+////            panelcentral.add(new JLabel("peleas"));
+//                panelDatos.add(new JLabel(rs.getString("peleas")));
+//                panelDatos.add(new JLabel(new ImageIcon(
+//                        BoxeadorView.class.getResource("/images/win.png"))));
+////            panelcentral.add(new JLabel("ganadas"));
+//                panelDatos.add(new JLabel(rs.getString("ganadas")));
+//                panelDatos.add(new JLabel(new ImageIcon(
+//                        BoxeadorView.class.getResource("/images/lose.png"))));
+////            panelcentral.add(new JLabel("perdidas"));
+//                panelDatos.add(new JLabel(rs.getString("perdidas")));
+//            }
 //        } catch (SQLException ex) {
-//            System.out.println(ex.getMessage());
+//            Logger.getLogger(BoxeadorView.class.getName()).log(Level.SEVERE, null, ex);
 //        }
+//        
+//    }
+    public void setValores() {
+
+        JPanel panelImg = new JPanel(new GridLayout(5, 1));
+        JPanel panelDatosBox = new JPanel(new GridLayout(5, 1));
+
+        try {
+            if (rs != null && !rs.isClosed()
+                    && !rs.isBeforeFirst() && !rs.isAfterLast()) {
+                add(panelDatosBox, BorderLayout.EAST);
+                panelImg.add(new JLabel(new ImageIcon(
+                        BoxeadorView.class.getResource("/images/nombre.png"))));
+//            panelcentral.add(new JLabel("nombre"));
+                panelDatosBox.add(new JLabel(rs.getString("nombre")));
+                panelImg.add(new JLabel(new ImageIcon(
+                        BoxeadorView.class.getResource("/images/mundo.png"))));
+//            panelcentral.add(new JLabel("nacionalidad"));
+                panelDatosBox.add(new JLabel(rs.getString("nacionalidad")));
+                panelImg.add(new JLabel(new ImageIcon(
+                        BoxeadorView.class.getResource("/images/peleas.png"))));
+//            panelcentral.add(new JLabel("peleas"));
+                panelDatosBox.add(new JLabel(rs.getString("peleas")));
+                panelImg.add(new JLabel(new ImageIcon(
+                        BoxeadorView.class.getResource("/images/win.png"))));
+//            panelcentral.add(new JLabel("ganadas"));
+                panelDatosBox.add(new JLabel(rs.getString("ganadas")));
+                panelImg.add(new JLabel(new ImageIcon(
+                        BoxeadorView.class.getResource("/images/lose.png"))));
+//            panelcentral.add(new JLabel("perdidas"));
+                panelDatosBox.add(new JLabel(rs.getString("perdidas")));
+
+//                anhadirImaxe();
+                Blob blob = rs.getBlob("foto");
+                InputStream inputStream = blob.getBinaryStream();
+                Image image = ImageIO.read(inputStream);
+                ImageIcon imageIcon = new ImageIcon(image);
+                panelFoto.add(new JLabel(imageIcon));
+
+            }
+        } catch (SQLException | IOException ex) {
+            Logger.getLogger(BoxeadorView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        panelDatos.add(panelFoto);
+        panelDatos.add(panelImg);
+        panelDatos.add(panelDatosBox);
+        add(panelDatos);
     }
 
+//    public void anhadirImaxe() {
+//        // Obtén el flujo de bytes de la base de datos
+//        InputStream inputStream;
+//        try {
+//            inputStream = rs.getBinaryStream(6);
+//
+//            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//            byte[] buffer = new byte[4096];
+//            int bytesRead;
+//
+//            try {
+//
+//                while ((bytesRead = inputStream.read(buffer)) != -1) {
+//                    outputStream.write(buffer, 0, bytesRead);
+//                }
+//
+//                byte[] imageBytes = outputStream.toByteArray();
+//                ImageIcon imageIcon = new ImageIcon(imageBytes);
+//
+//                JLabel imageLabel = new JLabel(imageIcon);
+//
+//                panelFoto.add(imageLabel);
+//            } catch (IOException e) {
+//                System.out.println(e.getMessage());
+//
+//            }
+//        } catch (SQLException ex) {
+//            Logger.getLogger(BoxeadorView.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
     public void setConnection() {
         try {
             Class.forName(DRIVER);
--- src/main/java/com/a22diegosg/proxectoprog/ProxectoProg.java
+++ src/main/java/com/a22diegosg/proxectoprog/ProxectoProg.java
@@ -15,12 +15,16 @@ import java.sql.Connection;
 public class ProxectoProg {
 
     public static void main(String[] args) {
-//        Connection con = ConectionManager.getConnection();
-//        BoxeadorDAO boxer = new BoxeadorDAO(con);
+        Connection con = ConectionManager.getConnection();
+        BoxeadorDAO boxer = new BoxeadorDAO(con);
 //        Boxeador big = new Boxeador("George Foreman", "USA", 81, 76, 5);
 //        boxer.create(big);
 //        boxer.actualizarFoto(big, new File("L:\\Programacion\\3ºTrimestre\\proxectoProg\\img\\foreman.jpg"));
 //        
+//          Boxeador ali = new Boxeador("Muhammad Ali", "USA", 61, 56, 5);
+//          boxer.create(ali);
+//          boxer.actualizarFoto(ali, new File("D:\\_Clase\\A22_Programacion\\proxectoProg\\img\\ali.jpg"));
+
 
         BoxeadorView bv = new BoxeadorView("Boxeador");
     }

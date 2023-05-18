d23b5396d3c2ebb1fc77732f7264a88e6108a102
ilizor <diegoseoges@gmail.com>
2023-05-18 21:28:20 +0200

    añadidos botones

--- src/main/java/com/a22diegosg/proxectoprog/BoxeadorView.java
+++ src/main/java/com/a22diegosg/proxectoprog/BoxeadorView.java
@@ -5,6 +5,8 @@
 package com.a22diegosg.proxectoprog;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.sql.*;
 import java.util.logging.Level;
@@ -23,84 +25,158 @@ public class BoxeadorView extends JFrame {
 
     private ResultSet rs;
 
+    JPanel panelcentral = new JPanel(new BorderLayout());
+    JPanel panelSur = new JPanel(new FlowLayout());
     JPanel panelDatos = new JPanel(new FlowLayout(FlowLayout.CENTER, 1, 1));
+    JPanel panelDatosBox = new JPanel(new GridLayout(5, 2));
+    JPanel panelFoto = new JPanel(new BorderLayout());
+
+    JButton seguinte;
+    JButton anterior;
 
     public BoxeadorView(String title) throws HeadlessException {
         super(title);
+        setConnection();
         crearGUI();
-        setConnection();        
         pack();
         setLocationRelativeTo(null);
+        setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
 
     public void crearGUI() {
-        add(new JPanel(), BorderLayout.PAGE_START);
-        add(new JPanel(), BorderLayout.PAGE_END);
-        setValores();
+        //setValores();
+
+        setMenu();
+
+        //panel sur
+        seguinte = new JButton(new ImageIcon(
+                BoxeadorView.class.getResource("/images/seguinte.png")));
+        seguinte.setBorderPainted(false);
+        seguinte.addActionListener((ActionEvent e) ->
+        {
+            try
+            {
+                if (rs.next()){
+                    setValores();                    
+                }
+            } catch (SQLException ex)
+            {
+                System.out.println("Erro no ActionListener: " + ex.getMessage());
+            }
+        });
+        seguinte.setContentAreaFilled(false);
+        anterior = new JButton(new ImageIcon(
+                BoxeadorView.class.getResource("/images/anterior.png")));
+        anterior.setBorderPainted(false);
+        anterior.setContentAreaFilled(false);
+        anterior.addActionListener((ActionEvent e) ->
+        {
+            try
+            {
+                if (rs.previous()){
+                    setValores();
+                }
+            } catch (SQLException ex)
+            {
+                System.out.println("Erro no ActionListener: " + ex.getMessage());
+            }
+        });
+        panelSur.add(anterior);
+        panelSur.add(seguinte);
+
+        panelcentral.add(panelSur, BorderLayout.SOUTH);
+        add(panelcentral, BorderLayout.CENTER);
     }
 
     public void setValores() {
 
-        
-        JPanel panelDatosBox = new JPanel(new GridLayout(5, 2));
-        JPanel panelFoto = new JPanel(new BorderLayout());
-
-        try {
+        try
+        {
             if (rs != null && !rs.isClosed()
-                    && !rs.isBeforeFirst() && !rs.isAfterLast()) {
+                    && !rs.isBeforeFirst() && !rs.isAfterLast())
+            {
+
                 add(panelDatosBox, BorderLayout.EAST);
-                panelDatosBox.add(new JLabel(new ImageIcon(
-                        BoxeadorView.class.getResource("/images/nombre.png"))));
-//            panelcentral.add(new JLabel("nombre"));
+                panelDatosBox.add(new JLabel("Nombre: ", new ImageIcon(
+                        BoxeadorView.class.getResource("/images/nombre.png")),
+                        SwingConstants.CENTER));
+
                 panelDatosBox.add(new JLabel(rs.getString("nombre")));
-                panelDatosBox.add(new JLabel(new ImageIcon(
-                        BoxeadorView.class.getResource("/images/mundo.png"))));
-//            panelcentral.add(new JLabel("nacionalidad"));
+                panelDatosBox.add(new JLabel("Nacion: ", new ImageIcon(
+                        BoxeadorView.class.getResource("/images/mundo.png")),
+                        SwingConstants.CENTER));
+
                 panelDatosBox.add(new JLabel(rs.getString("nacionalidad")));
-                panelDatosBox.add(new JLabel(new ImageIcon(
-                        BoxeadorView.class.getResource("/images/peleas.png"))));
-//            panelcentral.add(new JLabel("peleas"));
+                panelDatosBox.add(new JLabel("Peleas: ", new ImageIcon(
+                        BoxeadorView.class.getResource("/images/peleas.png")),
+                        SwingConstants.CENTER));
+
                 panelDatosBox.add(new JLabel(rs.getString("peleas")));
-                panelDatosBox.add(new JLabel(new ImageIcon(
-                        BoxeadorView.class.getResource("/images/win.png"))));
-//            panelcentral.add(new JLabel("ganadas"));
+                panelDatosBox.add(new JLabel("Ganadas: ", new ImageIcon(
+                        BoxeadorView.class.getResource("/images/win.png")),
+                        SwingConstants.CENTER));
+
                 panelDatosBox.add(new JLabel(rs.getString("ganadas")));
-                panelDatosBox.add(new JLabel(new ImageIcon(
-                        BoxeadorView.class.getResource("/images/lose.png"))));
-//            panelcentral.add(new JLabel("perdidas"));
+                panelDatosBox.add(new JLabel("Perdidas: ", new ImageIcon(
+                        BoxeadorView.class.getResource("/images/lose.png")),
+                        SwingConstants.CENTER));
+
                 panelDatosBox.add(new JLabel(rs.getString("perdidas")));
 
 //                anhadirImaxe();
                 byte[] imagen = rs.getBytes("FOTO");
-                if (imagen != null) {
-                    try {
+                if (imagen != null)
+                {
+                    try
+                    {
                         Image image = ImageIO.read(new ByteArrayInputStream(imagen));
                         ImageIcon imageIcon = new ImageIcon(image);
                         panelFoto.add(new JLabel(imageIcon));
-                    } catch (IOException e) {
+                    } catch (IOException e)
+                    {
                         System.out.println(e.getMessage());
                     }
+                } else
+                {
+                    setBackground(Color.BLACK); //NECESARIO COMPROBAR CON NULOS
                 }
-
             }
-        } catch (SQLException ex) {
+        } catch (SQLException ex)
+        {
             Logger.getLogger(BoxeadorView.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        panelDatos.add(panelFoto, BorderLayout.WEST);        
+        panelDatos.add(panelFoto, BorderLayout.WEST);
         panelDatos.add(panelDatosBox, BorderLayout.EAST);
-        add(panelDatos);
+        panelcentral.add(panelDatos);
+
+    }
+
+    public void setMenu() {
+        JMenuBar mnuBar = new JMenuBar();
+        JMenu menu = new JMenu();
+        JMenuItem guardar = new JMenuItem(new ImageIcon(
+                BoxeadorView.class.getResource("/images/guardar.png")));
+        ImageIcon icono = new ImageIcon(BoxeadorView.class.getResource(
+                "/images/menu.png"));
+        menu.setIcon(icono);
+        menu.add(guardar);
+        mnuBar.add(menu);
+        setJMenuBar(mnuBar);
     }
 
     public void setConnection() {
-        try {
+        try
+        {
             Class.forName(DRIVER);
-        } catch (ClassNotFoundException ex) {
+        } catch (ClassNotFoundException ex)
+        {
             System.out.println("Erro Drivers: " + ex.getMessage());
         }
-        try {
+        try
+        {
             Connection con = DriverManager.getConnection(URL);
             Statement st = con.createStatement(
                     ResultSet.TYPE_SCROLL_SENSITIVE,
@@ -108,10 +184,12 @@ public class BoxeadorView extends JFrame {
 
             rs = st.executeQuery("SELECT nombre, nacionalidad, peleas, "
                     + "ganadas, perdidas, foto FROM boxeador");
-            if (rs.next()) {
+            if (rs.next())
+            {
                 setValores();
             }
-        } catch (SQLException ex) {
+        } catch (SQLException ex)
+        {
             System.out.println("Erro na base de datos: " + ex.getMessage());
         }
     }

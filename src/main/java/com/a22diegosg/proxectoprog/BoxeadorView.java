ef478c0732bcb3c9d26f1ca06739b8de608ec283
ilizor <diegoseoges@gmail.com>
2023-05-19 17:01:52 +0200

    Non actualiza o boxeador

--- src/main/java/com/a22diegosg/proxectoprog/BoxeadorView.java
+++ src/main/java/com/a22diegosg/proxectoprog/BoxeadorView.java
@@ -34,9 +34,12 @@ public class BoxeadorView extends JFrame {
     JButton seguinte;
     JButton anterior;
 
+    JLabel nombreImg, nombre, nacImg, nac, peleasImg, peleas, ganadasImg,
+            ganadas, perdidasImg, perdidas, fotoLbl;
+
     public BoxeadorView(String title) throws HeadlessException {
         super(title);
-        setConnection();
+        setConnection();        
         crearGUI();
         pack();
         setLocationRelativeTo(null);
@@ -50,32 +53,59 @@ public class BoxeadorView extends JFrame {
 
         setMenu();
 
-        //panel sur
+        //PANEL NORTE
+        panelDatosBox.add(nombreImg);
+        panelDatosBox.add(nombre);
+        panelDatosBox.add(nacImg);
+        panelDatosBox.add(nac);
+        panelDatosBox.add(peleasImg);
+        panelDatosBox.add(peleas);
+        panelDatosBox.add(ganadasImg);
+        panelDatosBox.add(ganadas);
+        panelDatosBox.add(perdidasImg);
+        panelDatosBox.add(perdidas);
+
+        panelFoto.add(fotoLbl);
+        
+        //PANEL SUR
         seguinte = new JButton(new ImageIcon(
                 BoxeadorView.class.getResource("/images/seguinte.png")));
         seguinte.setBorderPainted(false);
+        seguinte.setContentAreaFilled(false);
+
+        anterior = new JButton(new ImageIcon(
+                BoxeadorView.class.getResource("/images/anterior.png")));
+        anterior.setBorderPainted(false);
+        anterior.setContentAreaFilled(false);
+        
+        
+
+        panelDatos.add(panelFoto, BorderLayout.WEST);
+        panelDatos.add(panelDatosBox, BorderLayout.EAST);
+        
+
         seguinte.addActionListener((ActionEvent e) ->
         {
             try
             {
-                if (rs.next()){
-                    setValores();                    
+                if (rs.next())
+                {
+                    System.out.println("seguinte");
+                    setValores();
                 }
             } catch (SQLException ex)
             {
                 System.out.println("Erro no ActionListener: " + ex.getMessage());
             }
         });
-        seguinte.setContentAreaFilled(false);
-        anterior = new JButton(new ImageIcon(
-                BoxeadorView.class.getResource("/images/anterior.png")));
-        anterior.setBorderPainted(false);
-        anterior.setContentAreaFilled(false);
+
         anterior.addActionListener((ActionEvent e) ->
         {
             try
             {
-                if (rs.previous()){
+                if (rs.previous())
+                {
+                    System.out.println("anterior");
                     setValores();
                 }
             } catch (SQLException ex)
@@ -86,85 +116,11 @@ public class BoxeadorView extends JFrame {
         panelSur.add(anterior);
         panelSur.add(seguinte);
 
-        panelcentral.add(panelSur, BorderLayout.SOUTH);
-        add(panelcentral, BorderLayout.CENTER);
-    }
-
-    public void setValores() {
-
-        try
-        {
-            if (rs != null && !rs.isClosed()
-                    && !rs.isBeforeFirst() && !rs.isAfterLast())
-            {
-
-                add(panelDatosBox, BorderLayout.EAST);
-                panelDatosBox.add(new JLabel("Nombre: ", new ImageIcon(
-                        BoxeadorView.class.getResource("/images/nombre.png")),
-                        SwingConstants.CENTER));
-
-                panelDatosBox.add(new JLabel(rs.getString("nombre")));
-                panelDatosBox.add(new JLabel("Nacion: ", new ImageIcon(
-                        BoxeadorView.class.getResource("/images/mundo.png")),
-                        SwingConstants.CENTER));
-
-                panelDatosBox.add(new JLabel(rs.getString("nacionalidad")));
-                panelDatosBox.add(new JLabel("Peleas: ", new ImageIcon(
-                        BoxeadorView.class.getResource("/images/peleas.png")),
-                        SwingConstants.CENTER));
-
-                panelDatosBox.add(new JLabel(rs.getString("peleas")));
-                panelDatosBox.add(new JLabel("Ganadas: ", new ImageIcon(
-                        BoxeadorView.class.getResource("/images/win.png")),
-                        SwingConstants.CENTER));
-
-                panelDatosBox.add(new JLabel(rs.getString("ganadas")));
-                panelDatosBox.add(new JLabel("Perdidas: ", new ImageIcon(
-                        BoxeadorView.class.getResource("/images/lose.png")),
-                        SwingConstants.CENTER));
-
-                panelDatosBox.add(new JLabel(rs.getString("perdidas")));
-
-//                anhadirImaxe();
-                byte[] imagen = rs.getBytes("FOTO");
-                if (imagen != null)
-                {
-                    try
-                    {
-                        Image image = ImageIO.read(new ByteArrayInputStream(imagen));
-                        ImageIcon imageIcon = new ImageIcon(image);
-                        panelFoto.add(new JLabel(imageIcon));
-                    } catch (IOException e)
-                    {
-                        System.out.println(e.getMessage());
-                    }
-                } else
-                {
-                    setBackground(Color.BLACK); //NECESARIO COMPROBAR CON NULOS
-                }
-            }
-        } catch (SQLException ex)
-        {
-            Logger.getLogger(BoxeadorView.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        panelDatos.add(panelFoto, BorderLayout.WEST);
-        panelDatos.add(panelDatosBox, BorderLayout.EAST);
         panelcentral.add(panelDatos);
+        
+        add(panelSur, BorderLayout.SOUTH);
+        add(panelcentral, BorderLayout.CENTER);
 
-    }
-
-    public void setMenu() {
-        JMenuBar mnuBar = new JMenuBar();
-        JMenu menu = new JMenu();
-        JMenuItem guardar = new JMenuItem(new ImageIcon(
-                BoxeadorView.class.getResource("/images/guardar.png")));
-        ImageIcon icono = new ImageIcon(BoxeadorView.class.getResource(
-                "/images/menu.png"));
-        menu.setIcon(icono);
-        menu.add(guardar);
-        mnuBar.add(menu);
-        setJMenuBar(mnuBar);
     }
 
     public void setConnection() {
@@ -193,5 +149,77 @@ public class BoxeadorView extends JFrame {
             System.out.println("Erro na base de datos: " + ex.getMessage());
         }
     }
+    public void setValores() {
+
+        try
+        {
+            if (rs != null && !rs.isClosed()
+                    && !rs.isBeforeFirst() && !rs.isAfterLast())
+            {
+
+                nombreImg = new JLabel("Nombre: ", new ImageIcon(
+                        BoxeadorView.class.getResource("/images/nombre.png")),
+                        SwingConstants.CENTER);
+                nombre = new JLabel(rs.getString("nombre"));
+
+                nacImg = new JLabel("Nacion: ", new ImageIcon(
+                        BoxeadorView.class.getResource("/images/mundo.png")),
+                        SwingConstants.CENTER);
+                nac = new JLabel(rs.getString("nacionalidad"));
+
+                peleasImg = new JLabel("Peleas: ", new ImageIcon(
+                        BoxeadorView.class.getResource("/images/peleas.png")),
+                        SwingConstants.CENTER);
+                peleas = new JLabel(rs.getString("peleas"));
+
+                ganadasImg = new JLabel("Ganadas: ", new ImageIcon(
+                        BoxeadorView.class.getResource("/images/win.png")),
+                        SwingConstants.CENTER);
+                ganadas = new JLabel(rs.getString("ganadas"));
+
+                perdidasImg = new JLabel("Perdidas: ", new ImageIcon(
+                        BoxeadorView.class.getResource("/images/lose.png")),
+                        SwingConstants.CENTER);
+                perdidas = new JLabel(rs.getString("perdidas"));
+
+                byte[] imagen = rs.getBytes("FOTO");
+                if (imagen != null)
+                {
+                    try
+                    {
+                        Image image = ImageIO.read(new ByteArrayInputStream(imagen));
+                        ImageIcon imageIcon = new ImageIcon(image);
+                        fotoLbl = new JLabel(imageIcon);
+                    } catch (IOException e)
+                    {
+                        System.out.println(e.getMessage());
+                    }
+                } else
+                {
+                    fotoLbl.setIcon(null); //NECESARIO COMPROBAR CON NULOS
+                }
+
+            }
+        } catch (SQLException ex)
+        {
+            Logger.getLogger(BoxeadorView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void setMenu() {
+        JMenuBar mnuBar = new JMenuBar();
+        JMenu menu = new JMenu();
+        JMenuItem guardar = new JMenuItem(new ImageIcon(
+                BoxeadorView.class.getResource("/images/guardar.png")));
+        ImageIcon icono = new ImageIcon(BoxeadorView.class.getResource(
+                "/images/menu.png"));
+        menu.setIcon(icono);
+        menu.add(guardar);
+        mnuBar.add(menu);
+        setJMenuBar(mnuBar);
+    }
+
+    
 
 }
